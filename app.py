ac30f266c45f3eeda35c305e94555a6971c01c21
aravindkumar00 <141475891+aravindkumar00@users.noreply.github.com>
2023-11-17 11:34:13 +0530

    Update app.py

--- app.py
+++ app.py
@@ -178,4 +178,4 @@ def store_candidateDetails(name, email, mobile_number):
     mysql.connection.commit()
     cur.close()
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000)
